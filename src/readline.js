a5aac14218b78171a764347caf24923454dda796
Olya Y <f_akora@mail.ru>
2023-06-26 18:13:54 +0300

    feat: add input processing on every enter in readline

--- src/readline.js
+++ src/readline.js
@@ -1,19 +1,29 @@
 import * as readline from 'node:readline';
 import { stdin as input, stdout as output } from 'node:process';
 
+import { userName } from './username.js';
+
 const launchReadline = async () => {
+  console.log('Please type your command.\n');
+
   const rl = readline.createInterface({ input, output });
 
-  rl.question('Please type your command.\n', (ans) => {
-    if (ans == 'yes') {
-      console.log(`Thank you for your valuable feedback: ${ans}`);
-    } else {
-      console.log('Closing');
+  rl.on('line', (input) => {
+    if (input == 'yes') {
+      console.log(`Thank you for your valuable feedback: ${input}\n`);
+    } else if (input === '.exit') {
+      console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
       rl.close();
+    } else {
+      console.log('Invalid input.\n');
     }
   });
   
-  
+  rl.on('SIGINT', () => {
+    console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
+    rl.close();
+  });
+
 }
 
 export default launchReadline;
