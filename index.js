c1bb949876442de3a098842ec9907b194c6a43fb
Olya Y <f_akora@mail.ru>
2023-06-26 18:25:42 +0300

    refactor: update the files structure

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-import { userName } from './src/username.js';
-import currentDir from './src/current-dir.js';
-import launchReadline from './src/readline.js';
+import { userName } from './src/general/username.js';
+import currentDir from './src/general/current-dir.js';
+import launchReadline from './src/general/readline.js';
 
 const startApp = async () => {  
   console.log(`Welcome to the File Manager, ${userName}!`);
   console.log(`You are currently in ${currentDir}`);
+
   await launchReadline();
 }
 
